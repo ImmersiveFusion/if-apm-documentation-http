3e81e35b0107ce2141bc20b5324b0263c1528a9b
gmneon <neon@gm.immersivefusion.net>
2023-11-22 10:55:50 -0500

    Ensure pip

--- envprep.ps1
+++ envprep.ps1
@@ -1,5 +1,7 @@
 # This scripts should not be run directly. Use serve.ps1 instead
 
+& python -m ensurepip --default-pip
+
 & python -m pip install --upgrade pip
 
 Write-Host Updating/installing mkdocs
