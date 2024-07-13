6549e931579672191606ccf506fdd755ea199c8c
neon <neon@gm.immersivefusion.net>
2024-07-13 17:36:38 -0400

    Highlight

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,4 +1,4 @@
 hljs.initHighlighting()
 
-//hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
-//hljs.registerLanguage('csharp', require('highlight.js/lib/languages/csharp'));
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('csharp', require('highlight.js/lib/languages/csharp'));
