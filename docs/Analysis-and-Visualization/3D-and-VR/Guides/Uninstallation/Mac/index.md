48eda5416e330f8c8e0887836fa0598091aa2714
Dan Kowalski <dkowalski@immersivefusion.com>
2025-10-13 12:39:36 -0400

    Mac and installation related.

--- docs/Analysis-and-Visualization/3D-and-VR/Guides/Uninstallation/Mac/index.md
+++ docs/Analysis-and-Visualization/3D-and-VR/Guides/Uninstallation/Mac/index.md
@@ -1,1 +1,6 @@
-# Mac (Coming soon)
+# Uninstalling on Map
+
+1. `Applications` on your computer.
+1. Find `Immersive APM` in the list of applications.
+1. Drag the `Immersive APM` to the `Trash`.
+1. All set!
--- docs/js/config.js
+++ docs/js/config.js
@@ -2,3 +2,4 @@ hljs.initHighlighting()
 
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 hljs.registerLanguage('csharp', require('highlight.js/lib/languages/csharp'));
+hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
